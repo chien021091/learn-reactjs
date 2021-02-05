ad416b21cdb0480258983ec6edc9a0800e362502
TRAN QUANG CHIEN <qctran1991@gmail.com>
2021-02-06 00:53:01 +0100

    form auth

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -14,8 +14,9 @@ InputField.propTypes = {
 function InputField(props) {
   const { form, name, label, disabled } = props;
   const { errors, formState } = form;
-  const hasError = formState.touched[name] && errors[name];
-  console.log(errors[name], formState.touched[name]);
+  //const hasError = formState.touched[name] && errors[name];
+  const hasError = errors[name];
+  //touched chi tinh khi da touche vao o input, neu muon luc nao cung hien thi loi thi remove touched
 
   return (
     <Controller
@@ -24,6 +25,8 @@ function InputField(props) {
       control={form.control}
       as={TextField} //tuong ung voi thanh phan nao trong material ui
       //nhung thu ben duoi se tu dong truyen vao ui text field
+      margin="normal"
+      variant="outlined"
       fullWidth
       label={label}
       disabled={disabled}
--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -1,11 +1,29 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, Button, makeStyles, Typography } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import InputField from '../../../../components/form-controls/InputField';
+import PasswordField from '../../../../components/form-controls/PasswordField';
+
+const useStyles = makeStyles(theme => ({
+  root : {
+    paddingTop : theme.spacing(4),
+  },
+  avatar : {
+    margin : '0 auto',
+    backgroundColor : theme.palette.secondary.main
+  },
+  title : {
+    textAlign : 'center',
+    margin : theme.spacing(2, 0, 3, 0),
+  },
+  submit : {
+    margin : theme.spacing(3, 0, 2, 0),
+  },
+}));
 
 RegisterForm.propTypes = {
   onSubmit: PropTypes.func,
@@ -13,15 +31,25 @@ RegisterForm.propTypes = {
 
 function RegisterForm(props) {
   const { onSubmit } = props;
+  const classes = useStyles();
 
   //them schema validation bat buoc
   const schema = yup.object().shape({
-    title: yup.string().required('Please enter title').min(5, 'Title is short'),
+    fullName: yup.string().required('Please enter full Name')
+    .test('should has at least two words', "Please enter at least two words", (value) => {
+      return value.split(' ').length >= 2;
+    }),
+    email : yup.string().required('Please enter email').email('Please enter an valid adresse'),
+    password : yup.string().required('Please enter password').min(6, 'Please enter at least 6 character'),
+    retypePassword : yup.string().required('Please retype password').oneOf([yup.ref('password')], 'Password does not match'),
   });
 
   const form = useForm({
     defaultValues: {
       title: '',
+      email: '',
+      password: '',
+      retypePassword: '',
     },
     resolver: yupResolver(schema), //validation o day
   });
@@ -32,15 +60,22 @@ function RegisterForm(props) {
   };
 
   return (
-    <div>
-        <Avatar>
-            <LockOutlined />
-        </Avatar>
-        <Typography component="h3" variant="h5">
-            Create An Account
-        </Typography>
+    <div className={classes.root}>
+      <Avatar className={classes.avatar}>
+        <LockOutlined />
+      </Avatar>
+      <Typography className={classes.title} component="h3" variant="h5">
+        Create An Account
+      </Typography>
       <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <InputField name="title" label="Todo" form={form} />
+        <InputField name="fullName" label="Full Name" form={form} />
+        <InputField name="email" label="Email" form={form} />
+        <PasswordField name="password" label="Password" form={form} />
+        <PasswordField name="retypePassword" label="Retype Password" form={form} />
+
+        <Button type="submit" className={classes.submit} variant='contained' color='primary' fullWidth>
+          Create An Account
+        </Button>
       </form>
     </div>
   );
