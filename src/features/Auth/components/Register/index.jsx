c94f3fad6d6cfca171e9e3bb421298eff88d1810
TRAN QUANG CHIEN <qctran1991@gmail.com>
2021-02-07 00:53:13 +0100

    form register

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RegisterForm from '../RegisterForm';
+import { useDispatch } from 'react-redux';
+import { register } from '../../userSlice';
+import { unwrapResult } from '@reduxjs/toolkit';
 
-Register.propTypes = {
-    
-};
+Register.propTypes = {};
 
 function Register(props) {
-    const handleSubmit = (values) => {
-        console.log('form submit', values);
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (values) => {
+    try {
+      values.username = values.email;
+      const action = register(values);
+      const resultAction = await dispatch(action);
+      const user = unwrapResult(resultAction);//lay ket qua ra vi async action se return ve 1 promise
+
+      console.log('new user', user);
+    } catch (error) {
+      console.log('fail to register', error);
     }
-    return (
-        <div>
-            <RegisterForm onSubmit = {handleSubmit} />
-        </div>
-    );
+  };
+
+  return (
+    <div>
+      <RegisterForm onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default Register;
---
+++ src/features/Auth/userSlice.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import userApi from '../../api/userApi';
+
+// First, create the thunk
+export const register = createAsyncThunk(
+    'user/register',
+    async (payload) => {
+      //call API to register
+      const data = await userApi.register(payload);
+
+      //save data to local storage
+      localStorage.setItem('access_token', data.jwt);
+      localStorage.setItem('user', JSON.stringify(data.user));
+
+      //return user data
+      return data.user;
+    }
+  )
+
+const userSlice = createSlice({
+    name : 'user',
+    initialState : {
+        current : {},
+        settings: {}
+    },
+    reducers : {}, //sync action
+    extraReducers : {
+        [register.fulfilled] : (state, action) => {
+            state.current = action.payload;
+        }
+    }
+});
+
+const { reducer } = userSlice;
+
+export default reducer;//default export
